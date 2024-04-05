d8b7a3ec05ee2115eadc55677559004a9ab797fd
M.P. Korstanje <mpkorstanje@users.noreply.github.com>
2024-04-05 15:29:20 +0200

    java: Implement TestNG XML formatter (#2)
    Implement a message based TestNG formatter.

---
+++ java/src/test/java/io/cucumber/testngxmlformatter/MessagesToTestngXmlWriterAcceptanceTest.java
@@ -0,0 +1,104 @@
+package io.cucumber.testngxmlformatter;
+
+import io.cucumber.messages.NdjsonToMessageIterable;
+import io.cucumber.messages.types.Envelope;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.xmlunit.builder.Input;
+
+import javax.xml.transform.Source;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static io.cucumber.testngxmlformatter.Jackson.OBJECT_MAPPER;
+import static org.xmlunit.assertj.XmlAssert.assertThat;
+
+class MessagesToTestngXmlWriterAcceptanceTest {
+    private static final NdjsonToMessageIterable.Deserializer deserializer = (json) -> OBJECT_MAPPER.readValue(json, Envelope.class);
+
+    static List<TestCase> acceptance() throws IOException {
+        try (Stream<Path> paths = Files.list(Paths.get("../testdata"))) {
+            return paths
+                    .filter(path -> path.getFileName().toString().endsWith(".ndjson"))
+                    .map(TestCase::new)
+                    .sorted(Comparator.comparing(testCase -> testCase.source))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("acceptance")
+    void test(TestCase testCase) throws IOException {
+        ByteArrayOutputStream bytes = writeTestngXmlReport(testCase, new ByteArrayOutputStream());
+        Source expected = Input.fromPath(testCase.expected).build();
+        Source actual = Input.fromByteArray(bytes.toByteArray()).build();
+        assertThat(actual).and(expected).ignoreWhitespace().areIdentical();
+    }
+
+    @ParameterizedTest
+    @MethodSource("acceptance")
+    @Disabled
+    void updateExpectedXmlReportFiles(TestCase testCase) throws IOException {
+        try (OutputStream out = Files.newOutputStream(testCase.expected)) {
+            writeTestngXmlReport(testCase, out);
+        }
+    }
+
+    private static <T extends OutputStream> T writeTestngXmlReport(TestCase testCase, T out) throws IOException {
+        try (InputStream in = Files.newInputStream(testCase.source)) {
+            try (NdjsonToMessageIterable envelopes = new NdjsonToMessageIterable(in, deserializer)) {
+                try (MessagesToTestngXmlWriter writer = new MessagesToTestngXmlWriter(out)) {
+                    for (Envelope envelope : envelopes) {
+                        writer.write(envelope);
+                    }
+                }
+            }
+        }
+        return out;
+    }
+
+    static class TestCase {
+        private final Path source;
+        private final Path expected;
+
+        private final String name;
+
+        TestCase(Path source) {
+            this.source = source;
+            String fileName = source.getFileName().toString();
+            this.name = fileName.substring(0, fileName.lastIndexOf(".ndjson"));
+            this.expected = source.getParent().resolve(name + ".xml");
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            TestCase testCase = (TestCase) o;
+            return source.equals(testCase.source);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(source);
+        }
+    }
+
+}
+
---
+++ java/src/test/java/io/cucumber/testngxmlformatter/MessagesToTestngXmlWriterTest.java
@@ -0,0 +1,97 @@
+package io.cucumber.testngxmlformatter;
+
+import io.cucumber.messages.types.Envelope;
+import io.cucumber.messages.types.TestRunFinished;
+import io.cucumber.messages.types.TestRunStarted;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.time.Instant;
+
+import static io.cucumber.messages.Convertor.toMessage;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class MessagesToTestngXmlWriterTest {
+
+    @Test
+    void it_writes_two_messages_to_xml() throws IOException {
+        Instant started = Instant.ofEpochSecond(10);
+        Instant finished = Instant.ofEpochSecond(30);
+
+        String html = renderAsJunitXml(
+                Envelope.of(new TestRunStarted(toMessage(started))),
+                Envelope.of(new TestRunFinished(null, true, toMessage(finished), null)));
+
+        assertThat(html).isEqualTo("" +
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<testng-results failed=\"0\" passed=\"0\" skipped=\"0\" total=\"0\">\n" +
+                "<suite name=\"Cucumber\" duration-ms=\"20000\">\n" +
+                "<test name=\"Cucumber\" duration-ms=\"20000\">\n" +
+                "</test>\n" +
+                "</suite>\n" +
+                "</testng-results>\n"
+        );
+    }
+
+    @Test
+    void it_writes_no_message_to_xml() throws IOException {
+        String html = renderAsJunitXml();
+        assertThat(html).isEqualTo("" +
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<testng-results failed=\"0\" passed=\"0\" skipped=\"0\" total=\"0\">\n" +
+                "<suite name=\"Cucumber\" duration-ms=\"0\">\n" +
+                "<test name=\"Cucumber\" duration-ms=\"0\">\n" +
+                "</test>\n" +
+                "</suite>\n" +
+                "</testng-results>\n"
+        );
+    }
+
+    @Test
+    void it_throws_when_writing_after_close() throws IOException {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        MessagesToTestngXmlWriter messagesToHtmlWriter = new MessagesToTestngXmlWriter(bytes);
+        messagesToHtmlWriter.close();
+        assertThrows(IOException.class, () -> messagesToHtmlWriter.write(null));
+    }
+
+    @Test
+    void it_can_be_closed_twice() throws IOException {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        MessagesToTestngXmlWriter messagesToHtmlWriter = new MessagesToTestngXmlWriter(bytes);
+        messagesToHtmlWriter.close();
+        assertDoesNotThrow(messagesToHtmlWriter::close);
+    }
+
+    @Test
+    void it_is_idempotent_under_failure_to_close() {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream() {
+            @Override
+            public void close() throws IOException {
+                throw new IOException("Can't close this");
+            }
+        };
+        MessagesToTestngXmlWriter messagesToHtmlWriter = new MessagesToTestngXmlWriter(bytes);
+        assertThrows(IOException.class, messagesToHtmlWriter::close);
+        byte[] before = bytes.toByteArray();
+        assertDoesNotThrow(messagesToHtmlWriter::close);
+        byte[] after = bytes.toByteArray();
+        assertThat(after).isEqualTo(before);
+    }
+
+
+    private static String renderAsJunitXml(Envelope... messages) throws IOException {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        try (MessagesToTestngXmlWriter messagesToHtmlWriter = new MessagesToTestngXmlWriter(bytes)) {
+            for (Envelope message : messages) {
+                messagesToHtmlWriter.write(message);
+            }
+        }
+
+        return new String(bytes.toByteArray(), UTF_8);
+    }
+}
