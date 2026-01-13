3493953747aeada88e9e59ccd027de92be333723
M.P. Korstanje <rien.korstanje@gmail.com>
2026-01-13 14:40:11 +0100

    Revert "Introduce module"
    This reverts commit 90aab6a3fdd05459d4808fc330aff1b03bfcd540.

--- java/src/main/java/module-info.java
+++ java/src/main/java/module-info.java
@@ -1,9 +1,7 @@
 module io.cucumber.testngxmlformatter {
     requires org.jspecify;
-
-    requires java.xml;
-
-    requires transitive io.cucumber.messages;
+    
+    requires io.cucumber.messages;
     requires io.cucumber.query;
 
     exports io.cucumber.testngxmlformatter;
