1f183699c2989734c29e97f54090a4c07a191ab7
M.P. Korstanje <rien.korstanje@gmail.com>
2026-01-15 03:39:22 +0100

    Reapply "Introduce module"
    This reverts commit adbd6966ef9b79de17d776b360b30dded73c1c6e.

---
+++ java/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module io.cucumber.testngxmlformatter {
+    requires org.jspecify;
+    
+    requires io.cucumber.messages;
+    requires io.cucumber.query;
+
+    exports io.cucumber.testngxmlformatter;
+}
